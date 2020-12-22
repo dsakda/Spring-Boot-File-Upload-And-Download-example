a4975d1deafd67a01e5afd9c5fdd8cc2ed036496
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-23 06:49:00 +0700

    Create SpringBootFileUploadDownloadExampleApplication.java

---
+++ src/main/java/com/example/SpringBootFileUploadDownloadExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadDownloadExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadDownloadExampleApplication.class, args);
+	}
+
+}
